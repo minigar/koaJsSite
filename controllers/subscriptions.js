dfb0017806d1f4ee15de0afeef6a2a3e720b266d
minigar <dd8556205@gmail.com>
2022-05-14 21:51:58 +0300

    add subscribe model and controller

---
+++ controllers/subscriptions.js
@@ -0,0 +1,45 @@
+const Router = require('koa-router');
+const passport = require('koa-passport');
+
+const Subscription = require('../models/Subscription');
+
+const router = new Router().prefix('/subscriptions');
+
+
+router.post('/', passport.authenticate('jwt', { session: false }), async(ctx) => {
+    const { profile } = ctx.request.body;
+    const subscriber_id = ctx.state.user._id; // current_user's id
+
+    if(profile !== subscriber_id.toString()) {
+        const checkSubscription = await Subscription.findOne({ subscriber_id, profile});
+    
+        if (checkSubscription) {
+            ctx.throw('You have already subscriped')
+        }
+    
+        ctx.body = await new Subscription({ subscriber_id, profile }).save();
+        ctx.status = 201;
+    }
+    else{
+        ctx.throw(400, "YYou can't follow yourself");
+    }
+
+
+});
+
+
+router.get('/', async(ctx) => {
+    ctx.body = await Subscription.find(ctx.query);
+});
+
+// '/:_id' : subscription id
+router.delete('/:_id', passport.authenticate('jwt', {session: false}), async (ctx) => {
+    await Subscription.findOneAndDelete({
+        _id: ctx.params._id,
+        subscriber_id: ctx.state.user._id
+    })
+    ctx.body = { message: 'You was unsubscribed' }
+})
+
+module.exports = router.routes();
+
