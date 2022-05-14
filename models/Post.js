0fbae4c4b0c06a67aec024365d797a997ea38c3e
minigar <dd8556205@gmail.com>
2022-05-14 21:54:47 +0300

    add populate Post model  to user and comments.user

--- models/Post.js
+++ models/Post.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const User = require('./User')
-
 
 const postSchema = new Schema({
     body: {
@@ -72,11 +70,6 @@ postSchema.post('save', function(doc, next) {
         next();
     });
 });
-
-// postSchema.post('save', async (doc, next) => {
-//     doc.populate(populationFields).execPopulate()
-//     next();
-// })
 
 function populateFields() {
     this.populate(populationFields)
