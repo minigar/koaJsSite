9f9ef7584ea6dd1315f9a7581398850a993db122
minigar <dd8556205@gmail.com>
2022-05-14 21:58:31 +0300

    almost not matter updates

--- handlers/index.js
+++ handlers/index.js
@@ -1,9 +1,11 @@
 const bodyParser = require('./body-parser');
 const errors = require("./errors");
 const passportInit = require('./passport -init');
+const catchMongooseErrors = require('./catch-mongoose-errors');
 
 module.exports = [
     bodyParser,
     errors,
-    passportInit
+    passportInit,
+    catchMongooseErrors
 ]
