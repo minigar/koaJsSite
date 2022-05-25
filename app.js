c1323ebd49be44d68bf8a329177c5be00270f62b
minigar <dd8556205@gmail.com>
2022-05-25 18:49:14 +0300

    --no-edit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 
 const Koa = require('koa');
+const jwtMiddleware = require("koa-jwt");
 
 const config = require('./lib/config');
 const handlers = require("./handlers");
@@ -11,11 +12,19 @@ const app = new Koa();
 
 handlers.forEach((h) => {app.use(h)});
 
+// Middleware below this line is only reached if JWT token is valid
+
 //Use the routes defined using the router
 app.use(controllers.routes());
 // allowed mehtods for controllers
 app.use(controllers.allowedMethods());
+
+
+
 // called function(module.exports = () => {}) from ../lib/mongoose-config.js
 mongooseConfig();
 
 app.listen(config.port, () => {console.log(`server has been started on port ${config.port}`)});
+
+
+module.exports = app;
