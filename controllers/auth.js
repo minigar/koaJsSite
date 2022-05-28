e95fec06822125ca6e34ec4ac36eb4f8cad175f8
minigar <dd8556205@gmail.com>
2022-05-28 03:00:21 +0300

    add refresh token route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,26 @@
-const Router = require('koa-router')
-const bcrypt = require('bcryptjs') // library for generate and compare password hash 
-const jwt = require('jsonwebtoken') //library for authentication 
+const Router = require('koa-router');
+const bcrypt = require('bcryptjs'); // library for generate and compare password hash 
+const jwt = require('jsonwebtoken'); //library for authentication 
 const { v4: uuidv4 } = require('uuid');
 
-const User = require('../models/User')
-const config = require('../lib/config')
+const jwtMethods = require('../helpers/jwtMethods');
+const User = require('../models/User');
+const Token = require('../models/Token');
+const config = require('../lib/config');
 
 const router = new Router().prefix('/auth')
+
+
+const updateTokens = (userId) => {
+    const accessToken = jwtMethods.generateAccessToken(userId);
+    const refreshToken = jwtMethods.generateRefreshToken();
+
+    return jwtMethods.replaceRefreshTokenInDb(refreshToken.id, userId)
+        .then(() => ({
+            accessToken,
+            refreshToken: refreshToken.token,
+        }));
+};
 
 
 router.post('/register', async (ctx) => {
@@ -30,6 +44,7 @@ router.post('/register', async (ctx) => {
 
 
 router.post('/login', async (ctx) => {
+    console.log(ctx.href);
     const { email, password } = ctx.request.body;
     const user = await User.findOne({ email });
 
@@ -40,27 +55,60 @@ router.post('/login', async (ctx) => {
     //compare password hash and user's password
     const comparePassword = await bcrypt.compare(password, user.password);
 
-    // if password equal password witch user writed, then create token
+    // if password equal password witch user writed, then create tokens
     if (comparePassword) {
-        const payload = {
-            id: user.id,
-            name: user.name,
-            email: user.email,
-        }
-
-        // create token
-        const token = jwt.sign(payload, config.secret, { expiresIn: 3600 * 24 });
-        const refreshToken = uuidv4();
-
-        //  for api
-        ctx.body = { token: `Bearer ${token}`,
-                    refreshToken};
+        // create access token and refresh token and writes in body
+        updateTokens(user._id)
+            .then(tokens => ctx.body = tokens)
     }
+
     else {
         ctx.throw(400, 'Password incorrect');
     }
 })
 
+
+
+router.post('/refresh', async (ctx) => {
+    console.log(ctx.href)
+    const { refreshToken } = ctx.request.body;
+    let payload;
+
+    try{
+        // verify token
+        payload = jwt.verify(refreshToken, config.jwt.secret);
+
+        if (payload.type !== 'refresh') {
+            ctx.throw(400, 'your token Invalid !')
+            return;
+        }
+    }
+
+    catch (e) {
+        if (e instanceof jwt.TokenExpiredError) {
+            ctx.throw(400, 'Expired token')
+        }
+
+        else if (e instanceof jwt.JsonWebTokenError) {
+            ctx.throw(400, 'your token Invalid!')
+            return;
+        }
+    }
+    
+    Token.findOne({ _id: payload.id })
+        .exec()
+        .then((token) => {
+            if (token === null) {
+                ctx.throw(400, 'Invalid token!');
+            }
+            // create new access token and new refresh token
+            return updateTokens(token.userId)
+        })
+        // write 2 new tokens in body 
+        .then(tokens => ctx.body = tokens)
+        .then(tokens => console.log(tokens))
+        .catch(err => console.log(err))
+})
 
 
 module.exports = router.routes();
